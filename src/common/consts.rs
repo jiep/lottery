7d04e208e91ab8215bdae6f620d99bdfd0905df2
José Ignacio Escribano <jiep@users.noreply.github.com>
2024-07-08 19:31:47 +0000

    Update draw id

--- src/common/consts.rs
+++ src/common/consts.rs
@@ -1,3 +1,3 @@
 pub const CHECK_URL: &str = "https://www.loteriasyapuestas.es/servicios/premioDecimoWeb?idsorteo=";
 pub const FIND_URL:  &str = "https://www.loteriasyapuestas.es/new-geo-web/JsonGenerationServlet/exportPois.txt?drawId={}&number=";
-pub const DEFAULT_DRAW_ID: u32 = 1222809102;
+pub const DEFAULT_DRAW_ID: u32 = 1259409102;
